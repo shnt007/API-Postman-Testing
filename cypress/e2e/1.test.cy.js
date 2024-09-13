b1eaf07e7462fb22668a66ef43b32af071ae0e0d
Shnt <92579864+shnt007@users.noreply.github.com>
2024-09-13 17:29:23 +0545

    Delete cypress directory

--- cypress/e2e/1.test.cy.js
+++
@@ -1,8 +0,0 @@
-describe('CSS Locator', () => {
-    it(' Search ', () => {
-        cy.visit("http://google.com.np")
-        cy.get('#APjFqb').type('facebook.com')
-        cy.get("div[class='lJ9FBc'] input[name='btnK']").click()
-        cy.xpath(`//a[@href="https://www.facebook.com/"]`).click()
-    })
-});
